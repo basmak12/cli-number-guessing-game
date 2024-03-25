4c6afd394cbdff073ce2a3fe83e7ecb567ea7293
basmak12 <164162260+basmak12@users.noreply.github.com>
2024-03-25 03:29:38 -0700

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+const randomNumber = Math.floor(Math.random() *6+1);
+
+
+const answers= await inquirer.prompt([
+    {
+        name: "UserGuessdNumber",
+        type: "number",
+        message:"please guess a number between 1-6:",
+    }
+]);
+
+if (answers.UserGuessdNumber === randomNumber) {
+    console.log("congratulations! you guessed a right number.");
+}else {
+    console.log("you guessed wrong number");
+ }
